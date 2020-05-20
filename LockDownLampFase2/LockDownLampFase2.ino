59342c2774f69e24a9fa3351b2a1ecd39f0008d6
Deepak Mehta <deep@deeeep.com>
2020-05-20 14:49:44 +0200

    Add files via upload

--- LockDownLampFase2/LockDownLampFase2.ino
+++ LockDownLampFase2/LockDownLampFase2.ino
@@ -1,30 +1,38 @@
+// Start with all the variables for each set of buttons and lights
+
+//Set1
 int LED1pin = 13;
 int Button1pin = 2;
 int button1State;
 int LED1State = LOW;
 int lastbutton1State = 0;
 
+//Set2
 int LED2pin = 12;
 int Button2pin = 4;
 int button2State;
 int LED2State = LOW;
 int lastbutton2State = 0;
 
+//Set3
 int LED3pin = 27;
 int Button3pin = 17;
 int button3State;
 int LED3State = LOW;
 int lastbutton3State = 0;
 
+//Set4
 int LED4pin = 33;
 int Button4pin = 19;
 int button4State;
 int LED4State = LOW;
 int lastbutton4State = 0;
 
+//Variables to make sure the button bounce is not seen as a switch
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 25;
 
+//define which pins do what
 void setup()
 {
   pinMode(LED1pin, OUTPUT);
@@ -48,20 +56,28 @@ void setup()
 
 void loop()
 {
+  //A repeat for each Set
+  //Read the button pin
   int reading = digitalRead(Button1pin);
+  //if button has changed
   if (reading != lastbutton1State) {
+    //how long ago was this
     lastDebounceTime = millis();
   }
-  
+
+  //if the button was pressed long enough
   if ((millis() - lastDebounceTime) > debounceDelay) {
     if (reading != button1State) {
       button1State = reading;
+      // if button is pressed
     if (button1State == HIGH) {
+      //change LED state
         LED1State = !LED1State;
       }
     }
   }
-  
+
+  //send led state to led pin for actual update of the LED
   digitalWrite(LED1pin, LED1State);
   lastbutton1State = reading;
 
@@ -107,7 +123,7 @@ void loop()
   if ((millis() - lastDebounceTime) > debounceDelay) {
     if (reading != button4State) {
       button4State = reading;
-      if (button4State == HIGH) {
+      if (button4State == HIGH && LED3State == LOW) {
         LED4State = !LED4State;
       }
     }
